d388f1e0ec1eada8eae1247a2dc097f52054837c
Syed Umair <syedumairandrabi66@gmail.com>
2020-03-31 10:39:23 +0530

    Add ViewPager2, PagerAdapter, TabLayout

---
+++ app/src/main/java/com/skapps/android/fightcovid/PagerAdapter.java
@@ -0,0 +1,31 @@
+package com.skapps.android.fightcovid;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+/**
+ * Created by Syed Umair on 31/03/2020.
+ */
+public class PagerAdapter extends FragmentStateAdapter {
+
+    public PagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+
+        if (position == 1) {
+            return new CountryFragment();
+        }
+        return new StateFragment();
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+}
