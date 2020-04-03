ef87e49bf2cb2a90a288251e9a5f98ac3d97dae4
irshad kasana <irshadgbl@gmail.com>
2020-04-03 13:18:45 +0530

    doWork (backgrounds)

--- app/src/main/java/com/skapps/android/fightcovid/MyWork.java
+++ app/src/main/java/com/skapps/android/fightcovid/MyWork.java
@@ -1,18 +1,36 @@
 package com.skapps.android.fightcovid;
 
+import android.app.Activity;
 import android.app.Application;
+import android.app.ListActivity;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.util.Log;
+import android.view.View;
 
 import com.skapps.android.fightcovid.ViewModels.JsonViewModel;
+import com.skapps.android.fightcovid.ViewModels.StateStatusVM;
 
+import java.sql.Wrapper;
 import java.util.List;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
+import static android.content.Context.MODE_PRIVATE;
 import static androidx.constraintlayout.widget.Constraints.TAG;
+import static com.skapps.android.fightcovid.LoginActivity.SHARED_PREFS1;
+import static com.skapps.android.fightcovid.QueryUtils.getTimeString;
 
 /**
  * Created by Irshad Kasana on 4/2/2020.
@@ -20,6 +38,7 @@ import static androidx.constraintlayout.widget.Constraints.TAG;
 public class MyWork extends Worker {
 
     JsonViewModel jsonViewModel;
+
 
    /// private static final String TAG = SyncDataWorker.class.getSimpleName();
 
@@ -32,7 +51,7 @@ public class MyWork extends Worker {
     @Override
     public Result doWork() {
 
-        Context applicationContext = getApplicationContext();
+     //   Context applicationContext = getApplicationContext();
         //simulate slow work
         // TODO
         //  WorkerUtils.makeStatusNotification("Fetching Data", applicationContext);
@@ -46,10 +65,48 @@ public class MyWork extends Worker {
             // calling the ViewModel
 
             // TODO
-            //  jsonViewModel = ViewModelProviders.of(this, new  JsonViewModel(this.getApplication(), "my awesome param")).get( JsonViewModel.class);
+            //  jsonViewModel = ViewModelProviders.of(this, new  JsonViewModel(getActivity().getApplication(), "my awesome param")).get( JsonViewModel.class);
+
+            new AsyncTask<String, String, String>() {
+                /**
+                 * Before starting background do some work.
+                 * */
+                @Override
+                protected void onPreExecute() {
+                }
+
+                @Override
+                protected String doInBackground(String... params) {
+                    // TODO fetch url data do bg process.
+                    return null;
+                }
+
+                /**
+                 * Update list ui after process finished.
+                 */
+                protected void onPostExecute(String result) {
+                    // NO NEED to use activity.runOnUiThread(), code execute here under UI thread.
+
+                    // Updating parsed JSON data into ListView
+                  // TODO PR1
+                    // remove the commit from next two lines and see the error
+                  //  StateStatusVM stateBarModel = new ViewModelProvider(this).get(StateStatusVM.class);
+                  //  stateBarModel.getData().observe(getViewLifecycleOwner(), new Observer<List<Integer>>() {
+                   //     @Override
+                     //   public void onChanged(List<Integer> integers) {
+                            //  lastUpdatedTv.setText(getTimeString(Integer.toString(integers.get(6)), Integer.toString(integers.get(7))));
+                        }
+                  //  });
+                    // not include
+                   // final List data = new Gson().fromJson(result);
+                    // updating listview
+                   // ((ListActivity) activity).updateUI(data);
+                } ;
+        }
 
 
-            //TODO
+
+                    //TODO
 //            if (response.isSuccessful() && response.body() != null && !response.body().isEmpty() && response.body().size() > 0) {
 ////
 ////                String data = WorkerUtils.toJson(response.body());
@@ -65,7 +122,7 @@ public class MyWork extends Worker {
 //            }
 
 
-        } catch (Throwable e) {
+        catch (Throwable e) {
             e.printStackTrace();
             // Technically WorkManager will return Result.failure()
             // but it's best to be explicit about it.
@@ -81,4 +138,19 @@ public class MyWork extends Worker {
         super.onStopped();
         Log.i(TAG, "OnStopped called for this worker");
     }
+
+
+//    private static Activity getActivity(Wrapper wrapper) {
+//        Activity activity;
+//        if (wrapper.getContext() instanceof android.app.Fragment) {
+//            activity = ((android.app.Fragment) wrapper.getContext()).getActivity();
+//        } else if (wrapper.getContext() instanceof android.support.v4.app.Fragment) {
+//            activity = ((android.support.v4.app.Fragment) wrapper.getContext()).getActivity();
+//        } else {
+//            activity = (Activity) wrapper.getContext();
+//        }
+//
+//        return activity;
+//    }
+
 }
